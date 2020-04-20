e7e3ed502dfecf5e61f5e362f4920a898c57c762
Timo Kökenhoff <timo@koekenhoff.net>
2020-04-21 01:18:14 +0200

    Repo creation

---
+++ README.rst
@@ -0,0 +1,27 @@
+# PIMaler
+
+## Small project derived from Make 01/2020 - the BrachioGraph
+
+-----
+TODO:
+-----
+ - add links here
+ - refer to original BrachioGraph as well
+ - add Adafruit or PCM class references
+ - sphinx to build docs?!
+
+
+### Sphinx
+
+$ pip install sphinx-rtd-theme
+
+conf.py needs to look like this
+
+import sphinx_rtd_theme
+
+extensions = [
+    ...
+    "sphinx_rtd_theme",
+]
+
+html_theme = "sphinx_rtd_theme"
---
+++ ServoController.py
@@ -0,0 +1,18 @@
+class Controller:
+
+    class __Controller:
+        def __init__(self, arg):
+            self.val = arg
+        def __str__(self):
+            return repr(self) + self.val
+
+    instance = None
+
+    def __init__(self, arg):
+        if not Controller.instance:
+            Controller.instance = Controller.__Controller(arg)
+        else:
+            Controller.instance.val = arg
+
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
